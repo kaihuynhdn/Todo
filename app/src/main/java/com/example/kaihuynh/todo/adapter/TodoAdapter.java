2c42bfeed9486fd21284569e57034d5a95abc9f1
kaihuynhdn <kaihuynhdn@gmail.com>
2018-03-30 09:45:49 +0700

    push

--- app/src/main/java/com/example/kaihuynh/todo/adapter/TodoAdapter.java
+++ app/src/main/java/com/example/kaihuynh/todo/adapter/TodoAdapter.java
@@ -1,12 +1,15 @@
 package com.example.kaihuynh.todo.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kaihuynh.todo.R;
@@ -36,6 +39,7 @@ public class TodoAdapter extends ArrayAdapter<Todo> {
             viewHolder = new ViewHolder();
             viewHolder.mTitle = convertView.findViewById(R.id.tv_title_todo_item);
             viewHolder.mDate =convertView.findViewById(R.id.tv_date_todo_item);
+            viewHolder.imageView = convertView.findViewById(R.id.img_item);
             convertView.setTag(viewHolder);
         }else {
             viewHolder = (ViewHolder) convertView.getTag();
@@ -43,11 +47,15 @@ public class TodoAdapter extends ArrayAdapter<Todo> {
 
         viewHolder.mTitle.setText(arrayList.get(position).getTitle());
         viewHolder.mDate.setText(arrayList.get(position).getDate());
+        byte[] img = arrayList.get(position).getImage();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(img, 0, img.length);
+        viewHolder.imageView.setImageBitmap(bitmap);
 
         return convertView;
     }
 
     class ViewHolder{
         private TextView mTitle, mDate;
+        private ImageView imageView;
     }
 }
